b5d7f2ca7bf9f32924cff4d140ed8db8f2315312
Le_PythonMan <87029220+Soporifiik7110@users.noreply.github.com>
2023-04-19 19:22:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from llama_cpp import Llama
+from colorama import Fore
+from Speaker import dire
+while True:
+    llm = Llama(model_path="D://AI_Research//python_binary_llama.cpp//ggml-vicuna-13b-4bit-rev1.bin", n_threads=3, use_mlock=True)
+    print(f"{Fore.GREEN}+++++++++++++++++++++++++++++ Your requests ++++++++++++++++++++++++++++++++\n")
+    req1 = input(f"\n{Fore.WHITE}Request: ")
+    req2 = input(f"\n{Fore.WHITE}Request_2:")
+    output = llm(
+        f"Question: {req1}?\nAnswer:", 
+        max_tokens=256,
+        temperature=0.8,
+        stop=["Question:", "\n"], echo=False)
+
+    output2 = llm(
+        f"Question: {req2}?\nAnswer:",
+        max_tokens=256,
+        temperature=0.8,
+        stop=["Question:", "\n"], echo=False)
+    
+    
+    print(Fore.GREEN + "\nThinking....................................................................\n")
+    print(Fore.CYAN + "Running Task1.........................................")
+    print(Fore.YELLOW + "++++++++++++++++++++++++++++ Task_1 +++++++++++++++++++++++++++++++++++++\n")
+    agent_text = output["choices"][0]["text"]
+    print(f"\n{Fore.WHITE} Task_1: \n{agent_text}")
+    dire(agent_text)
+    print(Fore.YELLOW + "\n+++++++++++++++++++++++++++++ End Task_1 +++++++++++++++++++++++++++++++++++++\n")
+    print(f"{Fore.RED}++++++++++++++++++++++++++++++++Task_2 ++++++++++++++++++++++++++++++++++++++++\n")
+    agent_text_2 = output2["choices"][0]["text"]
+    dire(agent_text_2)
+    print(f"\n{Fore.WHITE} Task_2: \n{agent_text_2}")
+    print(Fore.RED + "\n+++++++++++++++++++++++++++++ End Task_2 +++++++++++++++++++++++++++++++++++++\n")
+    
+
